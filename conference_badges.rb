d651dfeb225c07b882826277b230689df0919d84
exuberant-computer-4170 <dariusmoody61@gmail.com>
2020-02-23 19:28:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
+require 'pry'
 # Write your code here.
+
+def badge_maker(str)
+  # binding.pry
+  # puts "Hello my name is #{str}."
+   return "Hello my name is #{str}."
+end 
+
+def batch_badge_creator(arr)
+  new_arr = []
+  arr.each do |name|
+    #binding.pry
+    # new_arr.push ("Hello my name is #{name}.")
+    new_arr << "Hello my name is #{name}."
+  end 
+
+  new_arr
+end
